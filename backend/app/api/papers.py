0fada1109b8ab4916d9d949ef944646595ced5ee
Dhurv Gupta <dhurvgupta143@gmail.com>
2026-05-18 17:57:38 +0530

    fix user paper isolation

--- backend/app/api/papers.py
+++ backend/app/api/papers.py
@@ -1,26 +1,20 @@
-from fastapi import APIRouter
+from fastapi import (APIRouter,Depends)
 from app.database.connection import db
+from app.core.security import (get_current_user)
 
-router=APIRouter(
-    prefix="/papers",
-    tags=["Papers"]
-)
+router=APIRouter(prefix="/papers",tags=["Papers"])
 
-@router.get("")
-async def get_papers():
-
+@router.get("/")
+async def get_papers(current_user:dict=Depends(get_current_user)):
     papers=[]
 
-    async for paper in (
-        db.papers.find()
+    async for paper in (db.papers.find(
+            {
+                "user_id":current_user["id"]
+            }
+        )
     ):
-
-        paper["_id"]=str(
-            paper["_id"]
-        )
-
-        papers.append(
-            paper
-        )
+        paper["_id"]=str(paper["_id"])
+        papers.append(paper)
 
     return papers
