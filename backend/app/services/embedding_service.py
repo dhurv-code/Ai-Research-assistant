e5aab800b386ccbdeab84d60b922838b801083be
Dhurv Gupta <dhurvgupta143@gmail.com>
2026-09-20 09:35:09 +0530

    embedding

--- backend/app/services/embedding_service.py
+++ backend/app/services/embedding_service.py
@@ -1,6 +1,11 @@
-from chromadb.utils.embedding_functions import (
-    SentenceTransformerEmbeddingFunction
-)
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+
 
 class EmbeddingService:
 
@@ -10,10 +15,9 @@ class EmbeddingService:
     def get_embedding_function(cls):
 
         if cls.embedding_function is None:
-            cls.embedding_function = (
-                SentenceTransformerEmbeddingFunction(
-                    model_name="all-MiniLM-L6-v2"
-                )
+            cls.embedding_function = SentenceTransformerEmbeddingFunction(
+                model_name="all-MiniLM-L6-v2",
+                device="cpu"
             )
 
         return cls.embedding_function
@@ -33,3 +37,41 @@ class EmbeddingService:
         embedding_function = cls.get_embedding_function()
 
         return embedding_function([text])[0]
+
+
+
+# from chromadb.utils.embedding_functions import (
+#     SentenceTransformerEmbeddingFunction
+# )
+
+# class EmbeddingService:
+
+#     embedding_function = None
+
+#     @classmethod
+#     def get_embedding_function(cls):
+
+#         if cls.embedding_function is None:
+#             cls.embedding_function = (
+#                 SentenceTransformerEmbeddingFunction(
+#                     model_name="all-MiniLM-L6-v2"
+#                 )
+#             )
+
+#         return cls.embedding_function
+
+#     @classmethod
+#     def create_embeddings(cls, chunks):
+
+#         embedding_function = cls.get_embedding_function()
+
+#         embeddings = embedding_function(chunks)
+
+#         return embeddings
+
+#     @classmethod
+#     def create_query_embedding(cls, text):
+
+#         embedding_function = cls.get_embedding_function()
+
+#         return embedding_function([text])[0]
